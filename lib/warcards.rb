5c5a6f055d3bfed4cde2c89568c703b0a1e9553c
Robie Lutsey <robie1373@gmail.com>
2012-08-02 22:51:26 -0400

    Game works as desired. Yay. it can even be won or lost. Double yay.

--- lib/warcards.rb
+++ lib/warcards.rb
@@ -13,6 +13,9 @@ module Cardgame
   pch = Hash.new(0)
   ach = Hash.new(0)
 
+  # Set the game difficulty manually for now. Pick a number < 1. Higher is harder. TODO: make this a switch
+  difficulty = 0.4
+
   def self.get_questions
     file_name = "./test_question_file.txt"
     Querinator::Importer.new.parse(file_name)
@@ -35,25 +38,43 @@ module Cardgame
       result = game.foray.winner
       question = questions.sample
 
-      game.discard(result)
       if (result[:ai_cards].length + result[:player_cards].length) > 2
         puts "\n"
         puts '*@# There Was a WAR!! #@*'
         puts "\n"
-    end
+      end
+
       puts "-----------------------------------------------------------"
       puts "Round #{round}: #{result[:winner].name} won."
       puts "Player card was #{result[:player_cards].last.value} of #{result[:player_cards].last.suit}. #{game.player.name} has #{game.player.stack.length} cards in the stack and #{game.player.discard.length} cards on the discard"
       puts "AI card was #{result[:ai_cards].last.value} of #{result[:ai_cards].last.suit}. #{game.ai.name} has #{game.ai.stack.length} cards in the stack and #{game.ai.discard.length} cards on the discard"
       puts ""
-      p question.pose
-      reply = gets
-      reply = reply.chomp
-      if question.is_correct?(reply)
-        puts "That's right!!"
+
+      question_result_output = ""
+
+      if result[:winner] == game.ai
+        if rand < difficulty
+          result[:winner] = game.player
+          question_result_output << "\nBecuase #{game.ai.name} wos incorrect #{result[:winner].name} became the winner."
+        else
+          question_result_output << "#{game.ai.name} answered correctl!"
+        end
+
       else
-        puts "Ooooh. I'm sorry. The correct answer is '#{question.answer}'"
+        p question.pose
+        reply = gets
+        reply = reply.chomp
+        if question.is_correct?(reply)
+          question_result_output = "That's right!!"
+        else
+          question_result_output << "Ooooh. I'm sorry. The correct answer is '#{question.answer}'"
+          result[:winner] = game.ai
+          question_result_output << "\nBecuase the answer wos incorrect #{result[:winner].name} became the winner."
+        end
       end
+
+      game.discard(result)
+      puts question_result_output
       round += 1
       #pca = game.player.stack.length + game.player.discard.length
       #aca = game.ai.stack.length + game.ai.discard.length
